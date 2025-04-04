82f41a770f7f7ef9c0b7ca796da54f73038d8611
unknown <joaopereiradesignfnx@gmail.com>
2025-04-04 13:24:37 -0300

    adicionei * na regra de cors

--- server.js
+++ server.js
@@ -31,7 +31,12 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 // Middlewares
-app.use(cors());
+app.use(cors({
+  origin: '*',
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+}));
+
 app.use(express.json());
 app.use((req, res, next) => {
   console.log(`[${new Date().toISOString()}] ${req.method} ${req.url}`);
